44a96b5dc49bdf64e564f4b0762732b4c5d4e25a
Peliah <pelepoupa@gmail.com>
2023-08-23 12:22:36 +0100

    Hey mamah

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Home/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0074d9',
     alignItems: 'center',
     justifyContent: 'center',
   },
