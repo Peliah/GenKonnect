1e3d27f0a0ef1c229aff089c3c4318187c9c6bae
Peliah <pelepoupa@gmail.com>
2023-08-25 10:11:54 +0100

    Light bulb and context

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Screens/Home';
 import { AuthContextProvider } from './src/context/AuthContext';
-import AuthStack from './src/Navigation/AuthStack';
-import AppStack from './src/Navigation/AppStack';
+import {useFonts} from 'expo-font'
+import Router from './src/Navigation/Router';
 
 export default function App() {
   return (
     <AuthContextProvider>
-        <AppStack/>
+        <Router/>
     </AuthContextProvider>
   );
 }
